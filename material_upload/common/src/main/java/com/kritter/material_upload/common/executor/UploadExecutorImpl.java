7d078b5cc2d92a686e3864f38ad2ddb5fe1f2935
sunhaoming <sunhaoming@madhouse-inc.com>
2016-10-08 10:45:42 +0800

    update cloudcross material upload code

--- material_upload/common/src/main/java/com/kritter/material_upload/common/executor/UploadExecutorImpl.java
+++ material_upload/common/src/main/java/com/kritter/material_upload/common/executor/UploadExecutorImpl.java
@@ -21,278 +21,288 @@ import lombok.Getter;
 import lombok.Setter;
 
 public abstract class UploadExecutorImpl implements UploadExecutor {
-	private static final Logger LOG = LoggerFactory.getLogger(UploadExecutorImpl.class);
-	
-	@Getter@Setter
-	private boolean advertiser_upload=false;
-	@Getter@Setter
-	private boolean adposition_get=false;
-	@Getter@Setter
-	private boolean banner_upload=false;
-	@Getter@Setter
-	private boolean video_upload=false;
-	@Getter@Setter
-	private int pubIncId;
-	@Override
-	public void execute(Properties properties,Connection con) {
-	}
-	@Override
-	public void checkJobs(Properties properties,Connection con,int pubIncId) {
-		this.pubIncId = pubIncId;
-		PreparedStatement pstmt = null;
-		try{
-			pstmt =con.prepareStatement("select * from adxbasedexchanges_metadata where pubIncId=?");
-			pstmt.setInt(1, pubIncId);
-			ResultSet rset = pstmt.executeQuery();
-			if(rset.next()){
-				advertiser_upload=rset.getBoolean("advertiser_upload");
-				adposition_get=rset.getBoolean("adposition_get");
-				banner_upload=rset.getBoolean("banner_upload");
-				video_upload=rset.getBoolean("video_upload");
-			}
-		}catch(Exception e){
-			LOG.error(e.getMessage(),e);
-		}finally{
-			if(pstmt!= null){
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
-	@Override
-	public void executeAdpositionGet(Properties properties, AdPositionGet adPositionGet,Connection con,int pubincId) {
-		if(!this.adposition_get){
-			LOG.debug("PubIncId {} - ADPOSITION is not set in METADATA",pubincId);
-			return;
-		}
-		if(adPositionGet != null){
-			boolean autoCommit = true;
-			try{
-				autoCommit = con.getAutoCommit();
-				adPositionGet.init(properties);
-				adPositionGet.checkAdpositionGet(con, properties);
-				adPositionGet.getAdposition(properties);
-				con.setAutoCommit(false);
-				adPositionGet.insertAdposition(con, properties);
-				adPositionGet.updaterun(con, properties);
-				con.commit();
-			}catch(Exception e){
-				LOG.error(e.getMessage(),e);
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					LOG.error(e1.getMessage(),e1);				
-				}
-			}finally{
-				try {
-					con.setAutoCommit(autoCommit);
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
-	@Override
-	public void executeMaterialBannerUpload(Properties properties, MUBanner muBanner,Connection con,int pubincId) {
-		if(!this.banner_upload){
-			LOG.debug("PubIncId {} - BANNER UPLOAD is not set in METADATA",pubincId);
-			return;
-		}
+    private static final Logger LOG = LoggerFactory.getLogger(UploadExecutorImpl.class);
 
-		if(muBanner != null){
-			boolean autoCommit = true;
-			try{
-				autoCommit = con.getAutoCommit();
-				muBanner.init(properties);
-				con.setAutoCommit(false);
-				muBanner.getLastRun(properties, con);
-				muBanner.removeDisassociatedCreative(properties, con);
-				muBanner.getModifiedEntities(properties, con);
-				muBanner.insertOrUpdateBannerUpload(properties, con);
-				muBanner.uploadmaterial(properties, con);
-				muBanner.updaterun(properties, con);
-				con.commit();
-			}catch(Exception e){
-				LOG.error(e.getMessage(),e);
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					LOG.error(e1.getMessage(),e1);				
-				}
-			}finally{
-				try {
-					con.setAutoCommit(autoCommit);
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
-	@Override
-	public void executeMaterialBannerAudit(Properties properties, MUBannerAudit muBannerAudit,Connection con,int pubincId) {
-		if(!this.banner_upload){
-			LOG.debug("PubIncId {} - BANNERUPLOAD is not set in METADATA",pubincId);
-			return;
-		}
+    @Getter
+    @Setter
+    private boolean advertiser_upload = false;
+    @Getter
+    @Setter
+    private boolean adposition_get = false;
+    @Getter
+    @Setter
+    private boolean banner_upload = false;
+    @Getter
+    @Setter
+    private boolean video_upload = false;
 
-		if(muBannerAudit != null){
-			boolean autoCommit = true;
-			try{
-				autoCommit = con.getAutoCommit();
-				muBannerAudit.init(properties);
-				con.setAutoCommit(false);
-				muBannerAudit.fetchMaterialAudit(properties, con);
-				con.commit();
-			}catch(Exception e){
-				LOG.error(e.getMessage(),e);
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					LOG.error(e1.getMessage(),e1);				
-				}
-			}finally{
-				try {
-					con.setAutoCommit(autoCommit);
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
-	@Override
-	public void executeMaterialVideoUpload(Properties properties, MUVideo muVideo,Connection con,int pubincId) {
-		if(!this.video_upload){
-			LOG.debug("PubIncId {} - VIDEOUPLOAD is not set in METADATA",pubincId);
-			return;
-		}
+    @Override
+    public void execute(Properties properties, Connection con) {
+    }
 
-		if(muVideo != null){
-			boolean autoCommit = true;
-			try{
-				autoCommit = con.getAutoCommit();
-				muVideo.init(properties);
-				con.setAutoCommit(false);
-				muVideo.getLastRun(properties, con);
-				muVideo.removeDisassociatedCreative(properties, con);
-				muVideo.getModifiedEntities(properties, con);
-				muVideo.insertOrUpdateVideoUpload(properties, con);
-				muVideo.uploadmaterial(properties, con);
-				muVideo.updaterun(properties, con);
-				con.commit();
-			}catch(Exception e){
-				LOG.error(e.getMessage(),e);
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					LOG.error(e1.getMessage(),e1);				
-				}
-			}finally{
-				try {
-					con.setAutoCommit(autoCommit);
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
-	@Override
-	public void executeMaterialVideoAudit(Properties properties, MUVideoAudit muVideoAudit,Connection con,int pubincId) {
-		if(!this.video_upload){
-			LOG.debug("PubIncId {} - VIDEOUPLOAD is not set in METADATA",pubincId);
-			return;
-		}
+    @Override
+    public void checkJobs(Properties properties, Connection con, int pubIncId) {
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = con.prepareStatement("select * from adxbasedexchanges_metadata where pubIncId=?");
+            pstmt.setInt(1, pubIncId);
+            ResultSet rset = pstmt.executeQuery();
+            if (rset.next()) {
+                advertiser_upload = rset.getBoolean("advertiser_upload");
+                adposition_get = rset.getBoolean("adposition_get");
+                banner_upload = rset.getBoolean("banner_upload");
+                video_upload = rset.getBoolean("video_upload");
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
 
-		if(muVideoAudit != null){
-			boolean autoCommit = true;
-			try{
-				autoCommit = con.getAutoCommit();
-				muVideoAudit.init(properties);
-				con.setAutoCommit(false);
-				muVideoAudit.fetchMaterialAudit(properties, con);
-				con.commit();
-			}catch(Exception e){
-				LOG.error(e.getMessage(),e);
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					LOG.error(e1.getMessage(),e1);				
-				}
-			}finally{
-				try {
-					con.setAutoCommit(autoCommit);
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
-	@Override
-	public void executeAdvInfoUpload(Properties properties, MUAdvInfo advInfo,Connection con,int pubincId) {
-		if(!this.advertiser_upload){
-			LOG.debug("PubIncId {} - ADVINFOUPLOAD is not set in METADATA",pubincId);
-			return;
-		}
+    @Override
+    public void executeAdpositionGet(Properties properties, AdPositionGet adPositionGet, Connection con, int pubincId) {
+        if (!this.adposition_get) {
+            LOG.debug("PubIncId {} - ADPOSITION is not set in METADATA", pubincId);
+            return;
+        }
+        if (adPositionGet != null) {
+            boolean autoCommit = true;
+            try {
+                autoCommit = con.getAutoCommit();
+                adPositionGet.init(properties);
+                adPositionGet.checkAdpositionGet(con, properties);
+                adPositionGet.getAdposition(properties);
+                con.setAutoCommit(false);
+                adPositionGet.insertAdposition(con, properties);
+                adPositionGet.updaterun(con, properties);
+                con.commit();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    LOG.error(e1.getMessage(), e1);
+                }
+            } finally {
+                try {
+                    con.setAutoCommit(autoCommit);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
 
-		if(advInfo != null){
-			boolean autoCommit = true;
-			try{
-				autoCommit = con.getAutoCommit();
-				advInfo.init(properties);
-				con.setAutoCommit(false);
-				advInfo.getLastRun(properties, con);
-				advInfo.getModifiedEntities(properties, con);
-				advInfo.insertOrUpdateAdcInfoUpload(properties, con);
-				advInfo.uploadmaterial(properties, con);
-				advInfo.updaterun(properties, con);
-				con.commit();
-			}catch(Exception e){
-				LOG.error(e.getMessage(),e);
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					LOG.error(e1.getMessage(),e1);				
-				}
-			}finally{
-				try {
-					con.setAutoCommit(autoCommit);
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
-	@Override
-	public void executeMaterialAdvInfoAudit(Properties properties, MUADvInfoAudit muAdvInforAudit,Connection con) {
-		if(!this.advertiser_upload){
-			LOG.debug("PubIncId {} - ADVINFOUPLOAD is not set in METADATA",pubincId);
-			return;
-		}
+    @Override
+    public void executeMaterialBannerUpload(Properties properties, MUBanner muBanner, Connection con, int pubincId) {
+        if (!this.banner_upload) {
+            LOG.debug("PubIncId {} - BANNER UPLOAD is not set in METADATA", pubincId);
+            return;
+        }
 
-		if(muAdvInforAudit != null){
-			boolean autoCommit = true;
-			try{
-				autoCommit = con.getAutoCommit();
-				muAdvInforAudit.init(properties);
-				con.setAutoCommit(false);
-				muAdvInforAudit.fetchMaterialAudit(properties, con);
-				con.commit();
-			}catch(Exception e){
-				LOG.error(e.getMessage(),e);
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					LOG.error(e1.getMessage(),e1);				
-				}
-			}finally{
-				try {
-					con.setAutoCommit(autoCommit);
-				} catch (SQLException e) {
-					LOG.error(e.getMessage(),e);
-				}
-			}
-		}
-	}
+        if (muBanner != null) {
+            boolean autoCommit = true;
+            try {
+                autoCommit = con.getAutoCommit();
+                muBanner.init(properties);
+                con.setAutoCommit(false);
+                muBanner.getLastRun(properties, con);
+                muBanner.removeDisassociatedCreative(properties, con);
+                muBanner.getModifiedEntities(properties, con);
+                muBanner.insertOrUpdateBannerUpload(properties, con);
+                muBanner.uploadmaterial(properties, con);
+                muBanner.updaterun(properties, con);
+                con.commit();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    LOG.error(e1.getMessage(), e1);
+                }
+            } finally {
+                try {
+                    con.setAutoCommit(autoCommit);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void executeMaterialBannerAudit(Properties properties, MUBannerAudit muBannerAudit, Connection con, int pubincId) {
+        if (!this.banner_upload) {
+            LOG.debug("PubIncId {} - BANNERUPLOAD is not set in METADATA", pubincId);
+            return;
+        }
+
+        if (muBannerAudit != null) {
+            boolean autoCommit = true;
+            try {
+                autoCommit = con.getAutoCommit();
+                muBannerAudit.init(properties);
+                con.setAutoCommit(false);
+                muBannerAudit.fetchMaterialAudit(properties, con);
+                con.commit();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    LOG.error(e1.getMessage(), e1);
+                }
+            } finally {
+                try {
+                    con.setAutoCommit(autoCommit);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void executeMaterialVideoUpload(Properties properties, MUVideo muVideo, Connection con, int pubincId) {
+        if (!this.video_upload) {
+            LOG.debug("PubIncId {} - VIDEOUPLOAD is not set in METADATA", pubincId);
+            return;
+        }
+
+        if (muVideo != null) {
+            boolean autoCommit = true;
+            try {
+                autoCommit = con.getAutoCommit();
+                muVideo.init(properties);
+                con.setAutoCommit(false);
+                muVideo.getLastRun(properties, con);
+                muVideo.removeDisassociatedCreative(properties, con);
+                muVideo.getModifiedEntities(properties, con);
+                muVideo.insertOrUpdateVideoUpload(properties, con);
+                muVideo.uploadmaterial(properties, con);
+                muVideo.updaterun(properties, con);
+                con.commit();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    LOG.error(e1.getMessage(), e1);
+                }
+            } finally {
+                try {
+                    con.setAutoCommit(autoCommit);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void executeMaterialVideoAudit(Properties properties, MUVideoAudit muVideoAudit, Connection con, int pubincId) {
+        if (!this.video_upload) {
+            LOG.debug("PubIncId {} - VIDEOUPLOAD is not set in METADATA", pubincId);
+            return;
+        }
+
+        if (muVideoAudit != null) {
+            boolean autoCommit = true;
+            try {
+                autoCommit = con.getAutoCommit();
+                muVideoAudit.init(properties);
+                con.setAutoCommit(false);
+                muVideoAudit.fetchMaterialAudit(properties, con);
+                con.commit();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    LOG.error(e1.getMessage(), e1);
+                }
+            } finally {
+                try {
+                    con.setAutoCommit(autoCommit);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void executeAdvInfoUpload(Properties properties, MUAdvInfo advInfo, Connection con, int pubincId) {
+        if (!this.advertiser_upload) {
+            LOG.debug("PubIncId {} - ADVINFOUPLOAD is not set in METADATA", pubincId);
+            return;
+        }
+
+        if (advInfo != null) {
+            boolean autoCommit = true;
+            try {
+                autoCommit = con.getAutoCommit();
+                advInfo.init(properties);
+                con.setAutoCommit(false);
+                advInfo.getLastRun(properties, con);
+                advInfo.getModifiedEntities(properties, con);
+                advInfo.insertOrUpdateAdcInfoUpload(properties, con);
+                advInfo.uploadmaterial(properties, con);
+                advInfo.updaterun(properties, con);
+                con.commit();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    LOG.error(e1.getMessage(), e1);
+                }
+            } finally {
+                try {
+                    con.setAutoCommit(autoCommit);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void executeMaterialAdvInfoAudit(Properties properties, MUADvInfoAudit muAdvInforAudit, Connection con, int pubincId) {
+        if (!this.advertiser_upload) {
+            LOG.debug("PubIncId {} - ADVINFOUPLOAD is not set in METADATA", pubincId);
+            return;
+        }
+
+        if (muAdvInforAudit != null) {
+            boolean autoCommit = true;
+            try {
+                autoCommit = con.getAutoCommit();
+                muAdvInforAudit.init(properties);
+                con.setAutoCommit(false);
+                muAdvInforAudit.fetchMaterialAudit(properties, con);
+                con.commit();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    LOG.error(e1.getMessage(), e1);
+                }
+            } finally {
+                try {
+                    con.setAutoCommit(autoCommit);
+                } catch (SQLException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
 }
