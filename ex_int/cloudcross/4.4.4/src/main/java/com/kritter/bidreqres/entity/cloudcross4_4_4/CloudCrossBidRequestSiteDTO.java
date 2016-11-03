22d49c4acc7dd493fb0fc6f2ab8d9dffa58cb70e
hamlin <sunhaoming@madhouse-inc.com>
2016-11-03 17:47:17 +0800

    fix cloudcross field conversion exception

--- ex_int/cloudcross/4.4.4/src/main/java/com/kritter/bidreqres/entity/cloudcross4_4_4/CloudCrossBidRequestSiteDTO.java
+++ ex_int/cloudcross/4.4.4/src/main/java/com/kritter/bidreqres/entity/cloudcross4_4_4/CloudCrossBidRequestSiteDTO.java
@@ -1,8 +1,51 @@
 package com.kritter.bidreqres.entity.cloudcross4_4_4;
 
 import com.kritter.bidrequest.entity.common.openrtbversion2_3.BidRequestSiteDTO;
+import lombok.Setter;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CloudCrossBidRequestSiteDTO extends BidRequestSiteDTO {
+    @JsonProperty("cat")
+    @Setter
+    private String contentCategoriesForSite;
+
+    @JsonProperty("keywords")
+    @Setter
+    private String[] siteKeywordsCSV;
+
+    @JsonProperty("pagecat")
+    @Setter
+    private String pageContentCategories;
+
+    @JsonProperty("sectioncat")
+    @Setter
+    private String contentCategoriesSubsectionSite;
+
+    @Override
+    public String[] getContentCategoriesSubsectionSite() {
+        String[] strings = new String[1];
+        strings[0] = contentCategoriesSubsectionSite;
+        return strings;
+    }
+
+    @Override
+    public String[] getPageContentCategories() {
+        String[] strings = new String[1];
+        strings[0] = pageContentCategories;
+        return strings;
+    }
+
+    @Override
+    public String getSiteKeywordsCSV() {
+        return siteKeywordsCSV != null && siteKeywordsCSV.length > 0 ? siteKeywordsCSV[0] : "";
+    }
+
+    @Override
+    public String[] getContentCategoriesForSite() {
+        String[] strings = new String[1];
+        strings[0] = contentCategoriesForSite;
+        return strings;
+    }
 }
