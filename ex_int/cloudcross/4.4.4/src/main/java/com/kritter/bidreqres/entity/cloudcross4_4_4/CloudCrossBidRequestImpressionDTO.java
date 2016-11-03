e12fbc6249f8733441cadfa358d94407960d7d5a
hamlin <sunhaoming@madhouse-inc.com>
2016-11-03 15:58:45 +0800

    fix cloudcross mimes field type error

--- ex_int/cloudcross/4.4.4/src/main/java/com/kritter/bidreqres/entity/cloudcross4_4_4/CloudCrossBidRequestImpressionDTO.java
+++ ex_int/cloudcross/4.4.4/src/main/java/com/kritter/bidreqres/entity/cloudcross4_4_4/CloudCrossBidRequestImpressionDTO.java
@@ -1,10 +1,19 @@
 package com.kritter.bidreqres.entity.cloudcross4_4_4;
 
+import com.kritter.bidrequest.entity.common.openrtbversion2_3.BidRequestImpressionBannerObjectDTO;
 import com.kritter.bidrequest.entity.common.openrtbversion2_3.BidRequestImpressionDTO;
+import lombok.Setter;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CloudCrossBidRequestImpressionDTO extends BidRequestImpressionDTO {
+    @JsonProperty("banner")
+    @Setter
+    private CloudCrossBidRequestImpressionBannerObjectDTO bidRequestImpressionBannerObject;
 
-
+    @Override
+    public CloudCrossBidRequestImpressionBannerObjectDTO getBidRequestImpressionBannerObject() {
+        return bidRequestImpressionBannerObject;
+    }
 }
