b04754785d0d969d1fc0b9f95901362f31a40007
Kshitij <kshitij@kritter.in>
2017-03-10 12:20:30 +0530

    Changed to use budget instead of impression or click budget

--- bidder/bidding_strategies/cpc_cpm/rev_max/impl/kritter/create_model.py
+++ bidder/bidding_strategies/cpc_cpm/rev_max/impl/kritter/create_model.py
@@ -45,7 +45,6 @@ if __name__ == '__main__':
             bid = campaign.bid
             budgetRemaining = 0
             if bid > 0 and amountRemaining > 0:
-                budgetRemaining = amountRemaining / bid
                 budgetRemaining = budgetRemaining * 1000
             campaignBudget[campaignId] = budgetRemaining * (1 - cfg.networkCutRatio)
 
